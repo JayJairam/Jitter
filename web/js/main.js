fbc876a07e1f9788061fcecf8625c48a25267295
JayJairam <jmojairam@gmail.com>
2024-03-21 10:19:34 +0100

    scroll function done

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,14 @@
 fetch("./js/data.json")
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
         const frame = document.getElementById("loop");
-        data.forEach((element) => {
+        const scrollContainer = document.createElement("div"); 
+        scrollContainer.style.overflow = "auto"; 
+        scrollContainer.style.width = "100%"; 
+        scrollContainer.style.marginLeft = "65rem"; 
+        scrollContainer.style.display = "block"; 
+
+        data.forEach(element => {
             console.log(element);
             let newDiv = document.createElement("div");
             newDiv.id = element.id;
@@ -12,7 +18,9 @@ fetch("./js/data.json")
                 <p>${element.postPlaats}</p>
                 <img src="${element.imgPost}" alt="Post Image">
                 <p>${element.textPost}</p>`;
-            frame.appendChild(newDiv);
+            scrollContainer.appendChild(newDiv); 
         });
+        
+        frame.appendChild(scrollContainer); 
     })
-    .catch((error) => console.error("Error loading JSON file", error));
+    .catch(error => console.error("Error loading JSON file", error));
