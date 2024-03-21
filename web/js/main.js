95fcc8a9af90816ce16746db2bdeb468efa9db40
JayJairam <jmojairam@gmail.com>
2024-03-21 11:51:12 +0100

    les 5 routes

--- web/js/main.js
+++ web/js/main.js
@@ -2,11 +2,14 @@ fetch("./js/data.json")
     .then(response => response.json())
     .then(data => {
         const frame = document.getElementById("loop");
-        const scrollContainer = document.createElement("div"); 
-        scrollContainer.style.overflow = "auto"; 
-        scrollContainer.style.width = "100%"; 
-        scrollContainer.style.marginLeft = "65rem"; 
-        scrollContainer.style.display = "block"; 
+        const scrollContainer = document.createElement("div");
+        scrollContainer.style.overflow = "auto";
+        scrollContainer.style.width = "100%";
+        scrollContainer.style.marginLeft = "65rem";
+        scrollContainer.style.display = "block";
+
+        // Shuffle the data array
+        shuffleArray(data);
 
         data.forEach(element => {
             console.log(element);
@@ -18,9 +21,19 @@ fetch("./js/data.json")
                 <p>${element.postPlaats}</p>
                 <img src="${element.imgPost}" alt="Post Image">
                 <p>${element.textPost}</p>`;
-            scrollContainer.appendChild(newDiv); 
+            scrollContainer.appendChild(newDiv);
         });
-        
-        frame.appendChild(scrollContainer); 
+
+        frame.appendChild(scrollContainer);
     })
     .catch(error => console.error("Error loading JSON file", error));
+
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+
